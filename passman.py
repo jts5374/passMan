2b26813ee0a3e01c96a398f60addad44847b943d
jts5374 <jts5374@outlook.com>
2022-04-05 11:29:54 -0400

    Starting the project now

---
+++ passman.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+class User():
+    def __init__(self, username, mainpassword):
+        self.username = username
+        self.mainpassword = mainpassword
+        self.accounts = []
+    
+dir_path = os.path.dirname(os.path.realpath(__file__))
+con = sqlite3.connect(os.path.join(dir_path, 'passMan.db'))
+cur = con.cursor()
+cur.execute("""
+create table if not exists accounts(
+    userid integer Primary Key,   
+    username varchar(255) ,
+    password varchar(255))
+""")
+
+cur.execute("""
+insert into accounts(username, password)
+values('test', 'testing')
+""")
+
+con.commit()
