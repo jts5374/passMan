c237c914797b2ef2208b3bfb084c161b278c2180
jts5374 <jts5374@outlook.com>
2022-04-05 12:30:53 -0400

    sqlite3 stuff

--- encryptdecrypt.py
+++ encryptdecrypt.py
@@ -3,26 +3,24 @@ from cryptography.fernet import Fernet
 import bcrypt
 
 class password_and_salt():
-    def __init__(self, passwordandsalt):
-        self.hashpassword = passwordandsalt[29:]
-        self.salt = passwordandsalt[:29]
+    def __init__(self, plain_text_password):
+        password_and_salt = get_hashed_password_and_salt(plain_text_password)
+        self.hashpassword = password_and_salt[29:]
+        self.salt = password_and_salt[:29]
     
 
 def get_hashed_password_and_salt(plain_text_password):
-    # Hash a password for the first time
-    #   (Using bcrypt, the salt is saved into the hash itself)
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(plain_text_password, salt) 
 
 
 def check_password(plain_text_password, hashed_password):
-    # Check hashed password. Using bcrypt, the salt is saved into the hash itself
     return bcrypt.checkpw(plain_text_password, hashed_password)
 
 
-p = password_and_salt(get_hashed_password_and_salt('password'))
+# p = password_and_salt('password')
 
-print(p.hashpassword)
-print(p.salt)
+# print(p.hashpassword)
+# print(p.salt)
 
-print(check_password('password', p.salt+p.hashpassword))
+# print(check_password('password', p.salt+p.hashpassword))
--- passman.py
+++ passman.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import encryptdecrypt as ed
 
 class User():
     def __init__(self, username, mainpassword):
@@ -11,15 +12,27 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 con = sqlite3.connect(os.path.join(dir_path, 'passMan.db'))
 cur = con.cursor()
 cur.execute("""
-create table if not exists accounts(
-    userid integer Primary Key,   
-    username varchar(255) ,
+create table if not exists accounts(   
+    username varchar(255) Primary Key ,
     masterpassword varchar(255))
 """)
 
-cur.execute("""
-insert into accounts(username, password)
-values('test', 'testing')
-""")
+insertintoaccounts = """
+insert into accounts(username, masterpassword)
+values(?,?)
+"""
+
+
+
+getpassword = """
+select masterpassword from accounts where username = ?
+"""
+
+pwordquery = cur.execute(getpassword, ('jshea',))
+pword = pwordquery.fetchone()[0]
+print(ed.check_password('password', pword))
+
 
 con.commit()
+
+
